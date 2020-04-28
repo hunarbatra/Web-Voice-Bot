aa214f7f39e7651f24addd0a4856366e27489f8b
Hunar Batra <batra.hunar@gmail.com>
2020-04-28 14:57:05 +0530

    sixth commit

--- public/js/script.js
+++ public/js/script.js
@@ -8,17 +8,18 @@ const outputBot = document.querySelector('.output-bot');
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
+// recognition.continuous = true;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 document.querySelector('button').addEventListener('click', () => {
-  recognition.start();
-});
+    recognition.start();
+}); 
 
-document.getElementById('pause').addEventListener('click', () => {
+/* document.getElementById('pause').addEventListener('click', () => {
   recognition.stop();
-});
+}); */
 
 recognition.addEventListener('speechstart', () => {
   console.log('Speech has been detected.');
@@ -40,24 +41,29 @@ recognition.addEventListener('nomatch', (e) => {
   recognition.start();
 });
 
-/*
 recognition.addEventListener('speechend', () => {
   //recognition.stop();
-}); */ 
+}); 
 
 recognition.addEventListener('error', (e) => {
   outputBot.textContent = 'Error: ' + e.error;
 });
 
-function synthVoice(text) {
+function synthVoice(text, callback) {
   const synth = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance();
   utterance.text = text;
   synth.speak(utterance);
+  utterance.onend = function(event) {
+    recognition.start();
+    callback('ok');
+  }
 }
 
 socket.on('bot reply', function(replyText) {
-  synthVoice(replyText);
+  synthVoice(replyText, function(data) {
+    recognition.start();
+  });
 
   if(replyText == '') replyText = '(No answer...)';
   outputBot.textContent = replyText;
